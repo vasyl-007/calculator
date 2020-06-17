bde2d12686b111710daad680fe5e45528af5e6d1
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-17 20:38:20 +0300

    started to write calculator

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("hello calculator");
+
+function foo_0() {
+  const from_get = document.getElementById("id0").innerHTML;
+  console.log(from_get);
+}
+function foo_1() {
+  const from_get = document.getElementById("id1").innerHTML;
+  console.log(from_get);
+}
+function foo_2() {
+  const from_get = document.getElementById("id2").innerHTML;
+  console.log(from_get);
+}
+function foo_3() {
+  const from_get = document.getElementById("id3").innerHTML;
+  console.log(from_get);
+}
+function foo_4() {
+  const from_get = document.getElementById("id4").innerHTML;
+  console.log(from_get);
+}
+function foo_5() {
+  const from_get = document.getElementById("id5").innerHTML;
+  console.log(from_get);
+}
+function foo_6() {
+  const from_get = document.getElementById("id6").innerHTML;
+  console.log(from_get);
+}
+function foo_7() {
+  const from_get = document.getElementById("id7").innerHTML;
+  console.log(from_get);
+}
+function foo_8() {
+  const from_get = document.getElementById("id8").innerHTML;
+  console.log(from_get);
+}
+function foo_9() {
+  const from_get = document.getElementById("id9").innerHTML;
+  console.log(from_get);
+}
