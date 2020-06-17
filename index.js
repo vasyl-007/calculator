87c5abcc41e8bd168868cef2c2263e410edf1598
Vasyl-007 <vasyl.manyo@gmail.com>
2020-06-18 01:11:34 +0300

    added styles

--- index.js
+++ index.js
@@ -1,42 +1,110 @@
 console.log("hello calculator");
 
 function foo_0() {
-  const from_get = document.getElementById("id0").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id0").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
+  let count = document.getElementById("result").innerHTML;
+  count = count.length;
+
+  politel = count * 23;
+//   scores = 318 - politel;
+  scores = 330 - politel;
+  if (count === 1) {
+    scores = 295;
+  }
+  if (count === 2) {
+    scores = 272;
+  }
+  if (from_length > 12) {
+    fail;
+  }
+  document
+    .getElementById("result")
+    .setAttribute("style", "margin-left:" + scores + "px");
 }
 function foo_1() {
-  const from_get = document.getElementById("id1").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id1").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_2() {
-  const from_get = document.getElementById("id2").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id2").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_3() {
-  const from_get = document.getElementById("id3").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id3").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_4() {
-  const from_get = document.getElementById("id4").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id4").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_5() {
-  const from_get = document.getElementById("id5").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id5").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_6() {
-  const from_get = document.getElementById("id6").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id6").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_7() {
-  const from_get = document.getElementById("id7").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id7").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_8() {
-  const from_get = document.getElementById("id8").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id8").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
 function foo_9() {
-  const from_get = document.getElementById("id9").innerHTML;
-  console.log(from_get);
+  const from_id = document.getElementById("id9").innerHTML;
+  const from_result = document.getElementById("result").innerHTML;
+  if (from_result === "0") {
+    document.getElementById("result").innerHTML = "";
+  }
+  from_length = from_result.length;
+  document.getElementById("result").innerHTML += from_id;
 }
